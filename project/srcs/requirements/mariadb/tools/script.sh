581fc36959b279616b720714ddbb39ddeadec3ec
nel-brig <115668948+nel-brig1337@users.noreply.github.com>
2023-08-06 04:25:57 +0000

    Pending changes exported from your codespace

--- project/srcs/requirements/mariadb/tools/script.sh
+++ project/srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service mariadb start
 mariadb -u root -e "CREATE DATABASE wpdata; CREATE USER 'wpusr'@'%' IDENTIFIED BY 'wppass'; GRANT ALL PRIVILEGES ON wpdata.* TO 'wpusr'@'%'; FLUSH PRIVILEGES;"
-exec $@
+mysqld
